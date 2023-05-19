a2ec37e5d91c213975be3bad3521d8d01fe86f93
Epirex <estebanguzzo@gmail.com>
2023-05-19 01:36:14 -0300

    Removed cassette video, it's broken UI
    Signed-off-by: Epirex <estebanguzzo@gmail.com>

--- app/src/main/java/com/epirex/awesomemix/MainActivity.kt
+++ app/src/main/java/com/epirex/awesomemix/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.epirex.awesomemix
 
 import android.media.MediaPlayer
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
-import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
         val buttonPlay: Button = findViewById(R.id.buttonPlay)
         val buttonPause: Button = findViewById(R.id.buttonPause)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.awesomemixstatitc4)
 
         awesomeStart(buttonPlay, buttonPause)
     }
@@ -30,32 +31,16 @@ class MainActivity : AppCompatActivity() {
     private fun awesomeStart(buttonPlay: Button, buttonPause: Button) {
         val url = "https://stream.zeno.fm/0c5xvhqhedsvv"
         mediaPlayer = MediaPlayer()
-
         mediaPlayer.setDataSource(url)
-        mediaPlayer.setOnPreparedListener {
+        mediaPlayer.prepareAsync()
             buttonPlay.setOnClickListener {
-                val videoView = findViewById<VideoView>(R.id.videoView)
-                val videoPath = "android.resource://" + packageName + "/" + R.raw.cassette
-                val uri = Uri.parse(videoPath)
-                videoView.setVideoURI(uri)
-                videoView.setOnPreparedListener { mediaPlayer ->
-                    mediaPlayer.isLooping = true
-                }
                 mediaPlayer.start()
-                videoView.start()
                 Toast.makeText(this, "Reproduciendo radio", Toast.LENGTH_SHORT).show()
             }
 
-            buttonPause.setOnClickListener {
-                val videoView = findViewById<VideoView>(R.id.videoView)
-                val videoPath = "android.resource://" + packageName + "/" + R.raw.cassette
-                val uri = Uri.parse(videoPath)
-                videoView.setVideoURI(uri)
-                mediaPlayer.pause()
-                videoView.stopPlayback()
-                Toast.makeText(this, "En Pausa...", Toast.LENGTH_SHORT).show()
-            }
+        buttonPause.setOnClickListener {
+            mediaPlayer.pause()
+            Toast.makeText(this, "En Pausa...", Toast.LENGTH_SHORT).show()
         }
-        mediaPlayer.prepareAsync()
     }
 }
