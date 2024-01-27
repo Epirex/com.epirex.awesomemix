51abf1e6f16f152bfe78421dec75efd43b01f460
Epirex <estebanguzzo@gmail.com>
2024-01-27 17:28:07 -0300

    First implementation for Lottie

--- app/src/main/java/com/epirex/awesomemix/MainActivity.kt
+++ app/src/main/java/com/epirex/awesomemix/MainActivity.kt
@@ -3,22 +3,22 @@ package com.epirex.awesomemix
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private val mediaPlayer: MediaPlayer = MediaPlayer()
     private var isPlaying: Boolean = false
+    private lateinit var animationView: LottieAnimationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val buttonPlayer = findViewById<FloatingActionButton>(R.id.buttonPlayer)
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageResource(R.drawable.cassete)
+        animationView = findViewById(R.id.animationView)
 
         iniciarReproductor(buttonPlayer)
     }
@@ -36,10 +36,12 @@ class MainActivity : AppCompatActivity() {
         buttonPlayer.setOnClickListener {
             if (isPlaying) {
                 mediaPlayer.pause()
+                animationView.pauseAnimation()
                 Toast.makeText(this, "En pausa...", Toast.LENGTH_SHORT).show()
                 buttonPlayer.setImageResource(R.drawable.ic_play)
             } else {
                 mediaPlayer.start()
+                animationView.playAnimation()
                 Toast.makeText(this, "¡Rock and roll!", Toast.LENGTH_SHORT).show()
                 buttonPlayer.setImageResource(R.drawable.ic_pause)
             }
